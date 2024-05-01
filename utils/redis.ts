c1bfc8d167539dcc3ef69911bfa179b135d153f4
Jaafar Safawi <jsafawi2001@gmail.com>
2024-05-01 22:54:17 +0300

    fix Redis Max retires build

--- utils/redis.ts
+++ utils/redis.ts
@@ -1,10 +1,14 @@
-import {Redis} from 'ioredis';
+import { Redis } from 'ioredis';
 require('dotenv').config();
 
 const redisClient = () => {
-    if(process.env.REDIS_URL){
+    if (process.env.REDIS_URL) {
         console.log(`Redis connected`);
-        return process.env.REDIS_URL;
+        return {
+            host: 'still-lamb-42569.upstash.io',
+            port: 42569,
+            maxRetriesPerRequest: null  // This will disable retry limit
+        };
     }
     throw new Error('Redis connection failed');
 };
